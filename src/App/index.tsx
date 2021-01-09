0dfabb3bc68e5b7beadf5753aad5855234b96921
Yura Luchka <lluchkaa@gmail.com>
2021-01-09 13:39:39 +0200

    add app folder

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Routes from '@routes'
+
+type Props = {}
+
+class App extends React.Component<Props> {
+  render() {
+    return <Routes />
+  }
+}
+
+export default App
