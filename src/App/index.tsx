b5012267ac48bf10a4d9d1eb028522cc63fb0715
Yura Luchka <lluchkaa@gmail.com>
2021-01-09 16:05:14 +0200

    add configure

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,9 +5,29 @@ import reduxBoot from '@boot/redux'
 
 import Routes from '@routes'
 
+import {
+  configureReduxConsole,
+  configureScrollbarPadding,
+} from '@services/configure'
+
 type Props = {}
 
 class App extends React.Component<Props> {
+  private cleanup: (() => void) | undefined = undefined
+  constructor(props: Props) {
+    super(props)
+    const cleanupConsole = configureReduxConsole()
+    configureScrollbarPadding()
+
+    this.cleanup = () => {
+      cleanupConsole?.()
+    }
+  }
+
+  componentWillUnmount() {
+    this.cleanup?.()
+  }
+
   render() {
     return <Routes />
   }
