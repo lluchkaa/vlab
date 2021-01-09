a3dcdcecc710881e414c5a3edebc7b784c0d9c86
Yura Luchka <lluchkaa@gmail.com>
2021-01-09 13:39:19 +0200

    add empty pages

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+
+type Props = RouteComponentProps
+
+const Home: React.FC<Props> = () => null
+
+export default Home
