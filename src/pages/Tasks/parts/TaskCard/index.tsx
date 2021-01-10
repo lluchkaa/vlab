4e4e2530997fd6c8add8e823306fca16833f0387
AnastasiaTarasenko123 <anastasiyatarasenko2000@gmail.com>
2021-01-11 01:32:03 +0200

    remove console log

--- src/pages/Tasks/parts/TaskCard/index.tsx
+++ src/pages/Tasks/parts/TaskCard/index.tsx
@@ -8,28 +8,23 @@ interface Props {
   canRedirect?: boolean
 }
 
-const TaskCard: React.FC<Props> = ({ task, canRedirect = false }) => {
-  console.log('task', task)
-  console.log('canRedirect', canRedirect)
-
-  return (
-    <div className={styles['task-card-wrapper']}>
-      <p
-        className={clsx(
-          styles['task-card-title'],
-          canRedirect && styles['task-card-title-margin']
-        )}
-      >
-        {task.title}
-      </p>
-      <p className={styles['task-card-desc']}>{task.description}</p>
-      {canRedirect && (
-        <a href={`tasks/${task.id}`} className={styles['task-card-btn-link']}>
-          {'Виконати'}
-        </a>
+const TaskCard: React.FC<Props> = ({ task, canRedirect = false }) => (
+  <div className={styles['task-card-wrapper']}>
+    <p
+      className={clsx(
+        styles['task-card-title'],
+        canRedirect && styles['task-card-title-margin']
       )}
-    </div>
-  )
-}
+    >
+      {task.title}
+    </p>
+    <p className={styles['task-card-desc']}>{task.description}</p>
+    {canRedirect && (
+      <a href={`tasks/${task.id}`} className={styles['task-card-btn-link']}>
+        {'Виконати'}
+      </a>
+    )}
+  </div>
+)
 
 export default TaskCard
