44cf299f09ab952f2b66c156673552896d2e7723
Yura Luchka <lluchkaa@gmail.com>
2021-01-11 03:15:46 +0200

    add diagram

--- src/pages/Task/index.tsx
+++ src/pages/Task/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { RouteComponentProps } from 'react-router-dom'
+import * as go from 'gojs'
 
 import { getId, isId } from '@services/id'
 import links from '@routes/links'
@@ -9,6 +10,7 @@ import tasksActions from '@redux/tasks/actions'
 import useBindedAction from '@hooks/useBindedAction'
 
 import View from './view'
+import DiagramWrapper from './DiagramWrapper'
 
 interface PageParams {
   id: string
@@ -39,7 +41,29 @@ const Task: React.FC<Props> = ({ match: { params }, history: { replace } }) => {
     selectTask(id, undefined, () => replace(links.tasks()))
   }, [id])
 
-  return <View isLoading={isLoading} task={task} />
+  const linkNodes = useRef<go.ObjectData[]>(null)
+
+  const child = useMemo(
+    () =>
+      task ? (
+        <DiagramWrapper
+          nodeDataArray={[
+            { key: 0, text: 'Alpha', color: 'lightblue', loc: '0 0' },
+            { key: 1, text: 'Beta', color: 'orange', loc: '150 0' },
+            { key: 2, text: 'Gamma', color: 'lightgreen', loc: '0 150' },
+            { key: 3, text: 'Delta', color: 'pink', loc: '150 150' },
+          ]}
+          links={linkNodes}
+        />
+      ) : null,
+    [task]
+  )
+
+  return (
+    <View isLoading={isLoading} task={task}>
+      {child}
+    </View>
+  )
 }
 
 export default Task
--- src/pages/Task/view.tsx
+++ src/pages/Task/view.tsx
@@ -5,10 +5,11 @@ import Layout from '@common/Layout'
 interface Props {
   isLoading?: boolean
   task: Task | null
+  children?: React.ReactNode
 }
 
-const View: React.FC<Props> = ({ isLoading }) => (
-  <Layout showLoader={isLoading} />
+const View: React.FC<Props> = ({ isLoading, children }) => (
+  <Layout showLoader={isLoading}>{children}</Layout>
 )
 
 export default View
